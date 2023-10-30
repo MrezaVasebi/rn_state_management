1f94815af6ab545daeab900bf7a84d5a3402fd23
MrezaVasebi <reza.vaseb@gmail.com>
2023-10-30 11:47:20 +0330

    some files added

---
+++ utils/appColors.tsx
@@ -0,0 +1,13 @@
+export const appColors = {
+  grey: "grey",
+  white: "white",
+  red: "#d90429",
+  blue: "#0081a7",
+  bgColor: "#F5F5F5",
+  txtBack: "#e6e6e6",
+  txtPlaceColor: "#ccc5b9",
+
+  borderColors: {
+    grey: "grey",
+  },
+};
