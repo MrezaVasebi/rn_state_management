859f857c2d2f71f6527bfd33edee1adad323df3b
MrezaVasebi <reza.vaseb@gmail.com>
2023-11-05 09:00:52 +0330

    refactored: ui refactored

--- components/UserCart.tsx
+++ components/UserCart.tsx
@@ -1,8 +1,9 @@
 import { AntDesign, Entypo } from "@expo/vector-icons";
 import React from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { userType } from "../types/user_type";
 import { appColors } from "../utils";
+import AppButton from "./AppButton";
 import AppText from "./AppText";
 
 interface IItemRow {
@@ -21,26 +22,29 @@ const UserCart = ({
     return (
       <View style={styles.itemStyle}>
         <Entypo name={icon} size={20} color={appColors.grey} />
-        <AppText label={ans} lblStyle={{ marginLeft: 10 }} />
+
+        <AppText label={ans} lblStyle={styles.ansStyle} />
       </View>
     );
   };
 
   return (
     <View style={styles.rootStyle}>
-      <View style={{ alignItems: "flex-end" }}>
-        <TouchableOpacity
-          activeOpacity={0.5}
-          onPress={() => onDeleteUser(item.id)}
-        >
+      <View style={styles.itemContainer}>
+        <AppButton onPress={() => onDeleteUser(item.id)}>
           <AntDesign name="delete" color={appColors.red} size={25} />
-        </TouchableOpacity>
+        </AppButton>
       </View>
 
-      <ItemRow ans={item.fullName} icon="user" />
-      <ItemRow ans={item.mobile} icon="mobile" />
-      <ItemRow ans={item.email} icon="email" />
-      <ItemRow ans={item.address} icon="address" />
+      <View style={{ ...styles.eachRowStyle, marginBottom: 5 }}>
+        <ItemRow ans={item.fullName} icon="user" />
+        <ItemRow ans={item.mobile} icon="mobile" />
+      </View>
+
+      <View style={styles.eachRowStyle}>
+        <ItemRow ans={item.email} icon="email" />
+        <ItemRow ans={item.address} icon="address" />
+      </View>
     </View>
   );
 };
@@ -54,10 +58,22 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     backgroundColor: appColors.white,
   },
+  itemContainer: {
+    marginBottom: 10,
+    alignItems: "flex-end",
+  },
+  ansStyle: {
+    flex: 1,
+    marginLeft: 10,
+  },
   itemStyle: {
     flex: 1,
     flexDirection: "row",
     alignItems: "flex-start",
     marginBottom: 10,
+  },
+  eachRowStyle: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
